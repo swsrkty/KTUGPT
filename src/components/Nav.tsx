ac155ddf488f9644b73701be867f57ae429564dc
swsrkty <swsrkty@gmail.com>
2024-03-13 10:52:09 +0530

    Adding navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,16 @@
 "use-client"
 
+import { Button } from "@/components/ui/button"
 import { ModeToggle } from "./ui/toggle_button";
+
 
 export default function Nav() {
     return (
         <header>
-            <nav>
-                <ul className="flex items-center justify-between">
-                    <li>KTUGPT</li>
-                    <li><ModeToggle/></li>
+            <nav className="border-b-4 border-indigo-500 p-5">
+                <ul className="flex ">
+                    <li>KTUGPT</li> 
+                    <li className="fixed right-0"><Button>Login</Button></li>
                 </ul>
             </nav>
         </header>
