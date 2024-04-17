c65fb3ff5a469e8acac77be6074c29cb59d31231
MUHAMMED FARIZ KP <81973124+MUHAMMED-FARIZ-KP@users.noreply.github.com>
2024-04-17 15:45:23 +0530

    Edited UI (#6)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,11 +49,10 @@ export default function Home() {
         <HistorySidebar data={inputText}/>
         <section className="py-12 flex flex-col items-center text-center">
           <h1 className="text-4xl font-bold m-4 font-sans">Get answers from your textbooks</h1>
-          <p className="text-2xl text-muted-foreground">Select your subject and semester and start with your questions</p>
+        
         </section>
       <div className="flex gap-6 items-center justify-center">
-      < DropdownMenuCheckboxes2 />
-      < DropdownMenuCheckboxes />
+      
       
       </div>
       <div className="flex w-1/2 items-center justify-center space-x-2 mt-9 ml-96">
