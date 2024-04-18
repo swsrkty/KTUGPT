c7ec5df7cfcfae994e8ff6b16fe601f770ee8299
Sameemul Haque <110324374+sameemul-haque@users.noreply.github.com>
2024-04-18 11:49:15 +0530

    UI enhancements (#7)
    * set light theme as default
    
    * add useEffect for fetching data on inputText change
    fix sending empty inputText and previous inputText
    
    * update theme toggle button
    
    * update KTUGPT heading
    
    * update title and description
    
    * update style

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,10 +11,11 @@ export default function Nav() {
     const router = useRouter()
     return (
         <header>
-            <nav className="border-b-4 border-indigo-500 p-5">
+            <nav className="border-b-4 border-indigo-500 p-5 pb-7">
                 <ul className="flex ">
-                    <li>KTUGPT</li> 
-                    <li className="fixed right-5 -mt-2"><Button onClick={() => signOut(() => router.push("/"))}>Sign out</Button></li>
+                    <h1 className="text-[#0f172a] dark:text-white font-black font-sans text-3xl -mt-2 -mb-5">KTUGPT</h1>
+                    <li className="absolute right-28 -mt-2"><ModeToggle /></li>
+                    <li className="absolute right-5 -mt-2"><Button onClick={() => signOut(() => router.push("/"))}>Sign out</Button></li>
                 </ul>
             </nav>
         </header>
