2b5aa18854cde1f4bac1b8e4480b518aa8b45bd2
Sameemul Haque <110324374+sameemul-haque@users.noreply.github.com>
2024-04-19 12:43:37 +0530

    update chat history sidebar (#8)
    * feat: chat history showing even after refresh
    store the chat history locally in the browser's cache
    
    * feat: add clear chat history button
    - add confirm
    
    * fix: required attribute for question input
    
    * refactor
    
    * feat: add icon for clear chat history

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,74 +1,99 @@
-"use client"
-import * as React from "react"
-import { useState, useEffect, ChangeEvent } from "react"
-import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
+"use client";
+import * as React from "react";
+import { useState, useEffect, ChangeEvent } from "react";
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
-import { Input } from "@/components/ui/input"
-import Nav from "@/components/Nav"
-import { DropdownMenuCheckboxes } from "@/components/drop-down"
-import { DropdownMenuCheckboxes2 } from "@/components/drop-down2"
-import { Textarea } from "@/components/ui/textarea"
-import HistorySidebar from "@/components/ui/sidebar"
-type Checked = DropdownMenuCheckboxItemProps["checked"]
-
+import { Input } from "@/components/ui/input";
+import Nav from "@/components/Nav";
+import { DropdownMenuCheckboxes } from "@/components/drop-down";
+import { DropdownMenuCheckboxes2 } from "@/components/drop-down2";
+import { Textarea } from "@/components/ui/textarea";
+import HistorySidebar from "@/components/ui/sidebar";
+type Checked = DropdownMenuCheckboxItemProps["checked"];
 
 export default function Home() {
   const [inputText, setInputText] = useState([]);
   const [outputText, setOutputText] = useState("");
-  const [loading,setLoading]= useState(false);
+  const [loading, setLoading] = useState(false);
   const [source, setSource] = useState([]);
+
+  useEffect(() => {
+    const storedHistory = JSON.parse(
+      localStorage.getItem("chatHistory") || "[]"
+    );
+    setInputText(storedHistory);
+  }, []);
 
   useEffect(() => {
     if (inputText.length > 0) {
       setLoading(true);
       console.log(inputText.slice(-1));
-      fetch(` http://127.0.0.1:5000/?q=${inputText.slice(-1)}`, {  // Enter your IP address here
-        method: 'POST',
-        mode: 'cors',
+      fetch(` http://127.0.0.1:5000/?q=${inputText.slice(-1)}`, {
+        //====== Enter your IP address here ======
+        method: "POST",
+        mode: "cors",
       })
-      .then(response => response.text())
-      .then(text => {
-        text = JSON.parse(text);
-        setOutputText(text.result);
-        setLoading(false);
-        setSource(text.source);
-      })
-      .catch(error => console.error(error));
+        .then((response) => response.text())
+        .then((text) => {
+          text = JSON.parse(text);
+          setOutputText(text.result);
+          setLoading(false);
+          setSource(text.source);
+        })
+        .catch((error) => console.error(error));
     }
   }, [inputText]);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    setInputText(old => old.length < 1 ? [e.target.question.value] : [...old, e.target.question.value]);
+    const newInput = e.target.question.value;
+    setInputText((old) => [...old, newInput]);
+    localStorage.setItem(
+      "chatHistory",
+      JSON.stringify([...inputText, newInput])
+    );
+    // setInputText(old => old.length < 1 ? [e.target.question.value] : [...old, e.target.question.value]);
   };
 
-  
   return (
     <main className=" items-center justify-center">
-        <Nav/>
-        <HistorySidebar data={inputText}/>
-        <section className="flex flex-col pt-6 items-center text-center">
-          <h2 className="ml-12 text-4xl font-bold font-sans">Get answers from your textbooks</h2>
-        </section>
+      <Nav />
+      <HistorySidebar data={inputText} />
+      <section className="flex flex-col pt-6 items-center text-center">
+        <h2 className="ml-12 text-4xl font-bold font-sans">
+          Get answers from your textbooks
+        </h2>
+      </section>
       <div className="flex w-1/2 items-center justify-center space-x-2 mt-9 ml-96">
+        <form
+          onSubmit={handleChange}
+          className="flex w-full items-center justify-between"
+        >
+          <Input
+            id="question"
+            type="text"
+            placeholder="Enter your question here"
+            className="text-md w-full mr-2"
+            required
+          />
+          <Button type="submit">{loading ? <>Loading..</> : <>Go</>}</Button>
 
-        <form onSubmit={handleChange} className="flex w-full items-center justify-between">
-        
-        <Input id="question" type="text" placeholder="Enter your question here" className="text-md w-full mr-2" />
-        <Button type="submit" >{loading ? <>Loading..</> : <>Go</>}</Button>
-
-      {/* <p>Your input: {inputText.slice(-1)}</p> */}
+          {/* <p>Your input: {inputText.slice(-1)}</p> */}
         </form>
-        
       </div>
       <div className="flex-row justify-center items-center w-1/2 mt-9 ml-96 h-60">
-        <Textarea className="text-md rounded-xl align-center justify-center h-60" value={outputText}></Textarea>
+        <Textarea
+          className="text-md rounded-xl align-center justify-center h-60"
+          value={outputText}
+        ></Textarea>
       </div>
       <ul className="ml-96 pl-4 mt-2">
         <h1 className="font-bold">References:</h1>
-        {source.map(item => <li>{"Textbook: " + item.join(" - page: ")}</li>)}
+        {source.map((item) => (
+          <li>{"Textbook: " + item.join(" - page: ")}</li>
+        ))}
       </ul>
     </main>
   );
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './sidebar.css'; // You can remove this line if you no longer need the CSS file
+import React, { useState, useEffect } from "react";
+import { Trash2 } from 'lucide-react';
 interface HistoryItem {
   id: number;
   content: string;
@@ -9,31 +9,40 @@ function HistorySidebar(props) {
   const [historyItems, setHistoryItems] = useState<HistoryItem[]>([]);
 
   useEffect(() => {
-    // This code will be executed only on the client side
-  
     const fetchHistoryItems = async () => {
-      // Simulated data for demonstration purposes
-      const dummyHistory: HistoryItem[] = [
-    
-      ];
+      const dummyHistory: HistoryItem[] = [];
       setHistoryItems(dummyHistory);
     };
 
-    fetchHistoryItems(); // Execute the fetch function
-  }, [props]); // Empty dependency array means this effect runs only once, similar to componentDidMount
+    fetchHistoryItems();
+  }, [props]);
+
+  const clearChatHistory = () => {
+    window.confirm("Are you sure you want to clear chat history?") &&
+      (localStorage.removeItem("chatHistory"), window.location.reload());
+  };
 
   return (
     <div className="flex">
       <div className="w-65 bg-white-200 p-0 left-0 absolute h-full">
-        <div className='overflow-y-scroll h-screen w-60' id='scrollDiv'>
-        <h2 className="p-1 text-xl  text-white w-[180npx]  bg-[#0f172a] dark:bg-[#020817] text-center">Chat History</h2>
-        <ul className="list-none p-0 m-0">
-          {props.data.map(item => (
-            <li className="py-2 px-4 hover:bg-gray-300 dark:hover:bg-[#0f172a] dark:border-[#020817] border-b-4 border-r-4">{item}</li>
-          ))}
-        </ul>
+        <div className="overflow-y-scroll h-screen w-60" id="scrollDiv">
+          <h2 className="p-1 text-xl  text-white w-[180npx]  bg-[#0f172a] dark:bg-[#020817] text-center">
+            Chat History
+          </h2>
+          <button
+            className="float-right p-1 mr-1 hover:scale-105 transition-all ease-in-out"
+            onClick={clearChatHistory}
+          >
+            <Trash2 />
+          </button>
+          <ul className="list-none p-0 mt-8 dark:border-[#020817] border-t-4">
+            {props.data.map((item) => (
+              <li className="py-2 px-4 hover:bg-gray-300 dark:hover:bg-[#0f172a] dark:border-[#020817] border-b-4 border-r-4">
+                {item}
+              </li>
+            ))}
+          </ul>
         </div>
-        
       </div>
     </div>
   );
