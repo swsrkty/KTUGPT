8eb75a27377d7749d2342ff413f1d085cd37d9ae
Nuzaim <nuzaim.t123@gmail.com>
2024-04-29 18:06:42 +0530

    shows previous responses on clicking respective chat history

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,8 @@ type Checked = DropdownMenuCheckboxItemProps["checked"];
 
 export default function Home() {
   const [inputText, setInputText] = useState([]);
-  const [outputText, setOutputText] = useState("");
+  const [outputText, setOutputText] = useState([]);
+  const [id, setId] = useState(-1);
   const [loading, setLoading] = useState(false);
   const [source, setSource] = useState([]);
 
@@ -30,7 +31,7 @@ export default function Home() {
     if (inputText.length > 0) {
       setLoading(true);
       console.log(inputText.slice(-1));
-      fetch(` http://127.0.0.1:5000/?q=${inputText.slice(-1)}`, {
+      fetch(` http://127.0.0.1:5000/?q=${inputText.slice(-1)["text"]}`, {
         //====== Enter your IP address here ======
         method: "POST",
         mode: "cors",
@@ -38,8 +39,9 @@ export default function Home() {
         .then((response) => response.text())
         .then((text) => {
           text = JSON.parse(text);
-          setOutputText(text.result);
+          setOutputText((old) => [...old, text.result]);
           setLoading(false);
+          setId(-1);
           setSource(text.source);
         })
         .catch((error) => console.error(error));
@@ -49,18 +51,23 @@ export default function Home() {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     const newInput = e.target.question.value;
-    setInputText((old) => [...old, newInput]);
+    setInputText((old) => [...old, {"id": old.length+1, "text": newInput}]);
     localStorage.setItem(
       "chatHistory",
-      JSON.stringify([...inputText, newInput])
+      JSON.stringify([...inputText, {"id": inputText.length+1, "text": newInput}])
     );
-    // setInputText(old => old.length < 1 ? [e.target.question.value] : [...old, e.target.question.value]);
+    localStorage.setItem(
+      "chatResult",
+      JSON.stringify(outputText)
+    );
   };
+
+  const message = (itemId) => setId(itemId)
 
   return (
     <main className=" items-center justify-center">
       <Nav />
-      <HistorySidebar data={inputText} />
+      <HistorySidebar data={inputText} selectHistory={message}/>
       <section className="flex flex-col pt-6 items-center text-center">
         <h2 className="ml-12 text-4xl font-bold font-sans">
           Get answers from your textbooks
@@ -86,7 +93,7 @@ export default function Home() {
       <div className="flex-row justify-center items-center w-1/2 mt-9 ml-96 h-60">
         <Textarea
           className="text-md rounded-xl align-center justify-center h-60"
-          value={outputText}
+          value={id == -1 ? outputText.slice(-1) : outputText[id-1]}
         ></Textarea>
       </div>
       <ul className="ml-96 pl-4 mt-2">
