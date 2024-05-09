0bd886abfeb82e4d654e7782a26f1dec4a71fb9c
Sameemul Haque <110324374+sameemul-haque@users.noreply.github.com>
2024-05-09 09:58:53 +0530

    feat: responsive (#11)
    * feat: responsive
    
    * fix
    
    * fix url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
     if (inputText.length > 0) {
       setLoading(true);
       console.log(inputText.slice(-1));
-      fetch(` https://sameemul-haque-ktugpt.hf.space//?q=${inputText[inputText.length-1]["text"]}`, {
+      fetch(` https://sameemul-haque-ktugpt.hf.space/?q=${inputText[inputText.length-1]["text"]}`, {
         //====== Enter your IP address here ======
         method: "POST",
         mode: "cors",
@@ -69,47 +69,51 @@ export default function Home() {
   const message = (itemId) => setId(itemId)
 
   return (
-    <main className=" items-center justify-center">
+    <main>
       <Nav />
-      <HistorySidebar data={inputText} selectHistory={message}/>
-      <section className="flex flex-col pt-6 items-center text-center">
-        <h2 className="ml-12 text-4xl font-bold font-sans">
-          Get answers from your textbooks
-        </h2>
-      </section>
-      <div className="flex w-1/2 items-center justify-center space-x-2 mt-9 ml-96">
-        <form
-          onSubmit={handleChange}
-          className="flex w-full items-center justify-between"
-        >
-          <Input
-            id="question"
-            type="text"
-            placeholder="Enter your question here"
-            className="text-md w-full mr-2"
-            required
-          />
-          <Button type="submit" disabled={loading}>{loading ? <>Loading..</> : <>Go</>}</Button>
+      <div className="flex">
+        <HistorySidebar data={inputText} selectHistory={message}/>
+        <section className="grow">
+          <h2 className="pt-8 lg:pt-6 text-center text-2xl lg:text-3xl font-bold font-sans">
+            Get answers from your textbooks
+          </h2>
+          <div className="flex justify-center">
+            <form
+              onSubmit={handleChange}
+              className="flex flex-row gap-4 pt-6 w-10/12 lg:w-1/2"
+              >
+              <Input
+                id="question"
+                type="text"
+                placeholder="Enter your question here"
+                className="md:text-md"
+                required
+                />
+              <Button type="submit" disabled={loading}>{loading ? <>Loading..</> : <>Go</>}</Button>
 
-          {/* <p>Your input: {inputText.slice(-1)}</p> */}
-        </form>
+              {/* <p>Your input: {inputText.slice(-1)}</p> */}
+            </form>
+          </div>
+          <div className="flex justify-center pt-6 h-52">
+            <Textarea
+              className="text-md rounded-xl w-10/12 lg:w-1/2 h-52"
+              value={id == -1 ? outputText.slice(-1) : outputText[id-1]}
+              ></Textarea>
+          </div>
+          <div className="flex justify-center pt-6">
+            <ul className="w-10/12 lg:w-1/2">
+              <h1 className="font-bold">References:</h1>
+              {
+                source.length > 0 ?
+                source[id == -1 ? source.length - 1 : id - 1].map((item) => (
+                  <li>{"Textbook: " + item.join(" - page: ")}</li>
+                )):
+                null
+              }
+            </ul>
+          </div>
+        </section>
       </div>
-      <div className="flex-row justify-center items-center w-1/2 mt-9 ml-96 h-60">
-        <Textarea
-          className="text-md rounded-xl align-center justify-center h-60"
-          value={id == -1 ? outputText.slice(-1) : outputText[id-1]}
-        ></Textarea>
-      </div>
-      <ul className="ml-96 pl-4 mt-2">
-        <h1 className="font-bold">References:</h1>
-        {
-          source.length > 0 ?
-          source[id == -1 ? source.length - 1 : id - 1].map((item) => (
-          <li>{"Textbook: " + item.join(" - page: ")}</li>
-          )):
-          null
-        }
-      </ul>
     </main>
   );
 }
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Trash2 } from 'lucide-react';
+import { Trash2, ChevronRight, ChevronLeft  } from 'lucide-react';
 interface HistoryItem {
   id: number;
   content: string;
@@ -7,6 +7,7 @@ interface HistoryItem {
 
 function HistorySidebar(props) {
   const [historyItems, setHistoryItems] = useState<HistoryItem[]>([]);
+  const [showSidebar, setShowSidebar] = useState(false);
 
   useEffect(() => {
     const fetchHistoryItems = async () => {
@@ -17,6 +18,10 @@ function HistorySidebar(props) {
     fetchHistoryItems();
   }, [props]);
 
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   const clearChatHistory = () => {
     window.confirm("Are you sure you want to clear chat history?") &&
       (localStorage.removeItem("chatHistory"),
@@ -26,33 +31,42 @@ function HistorySidebar(props) {
       );
   };
 
+
   return (
-    <div className="flex">
-      <div className="w-65 bg-white-200 p-0 left-0 absolute h-full">
-        <div className="overflow-y-scroll h-screen w-60" id="scrollDiv">
-          <h2 className="p-1 text-xl  text-white w-[180npx]  bg-[#0f172a] dark:bg-[#020817] text-center">
-            Chat History
-          </h2>
-          <button
-            className="float-right p-1 mr-1 hover:scale-105 transition-all ease-in-out"
-            onClick={clearChatHistory}
-          >
-            <Trash2 />
-          </button>
-          <ul className="list-none p-0 mt-8 dark:border-[#020817] border-t-4">
-            {props.data.map((item) => (
-              <li 
-                className="py-2 px-4 hover:bg-gray-300 dark:hover:bg-[#0f172a] dark:border-[#020817] border-b-4 border-r-4" 
-                key={item.id}
-                onClick={() => props.selectHistory(item.id)}
-              >
-                {item.text}
-              </li>
-            ))}
-          </ul>
+    <>
+      <button className="flex absolute m-0.5 lg:hidden" >
+        <ChevronRight onClick={toggleSidebar} size={32} className="text-white rounded bg-[#0f172a] dark:bg-[#020817] p-1 hover:text-gray-300"/>
+      </button>
+      <div className={"lg:flex" + (showSidebar ? " flex" : " hidden")}>
+        <div className="w-65 bg-white dark:bg-[#0f172a] p-0 left-0 absolute h-full">
+          <div className="overflow-y-scroll h-screen w-60 border-r-4" id="scrollDiv">
+            <button className="lg:hidden float-right p-1 mr-1 mt-0.5 transition-all ease-in-out">
+              <ChevronLeft onClick={toggleSidebar} className="text-white hover:text-gray-300"/>
+            </button>
+            <h2 className="p-1 text-xl  text-white w-[180npx]  bg-[#0f172a] dark:bg-[#020817] text-center">
+              Chat History
+            </h2>
+            <button
+              className="float-right p-1 mr-1 hover:scale-105 transition-all ease-in-out"
+              onClick={clearChatHistory}
+            >
+              <Trash2 />
+            </button>
+            <ul className="list-none p-0 mt-8 dark:border-[#020817] border-t-4">
+              {props.data.map((item) => (
+                <li 
+                  className="py-2 px-4 hover:bg-gray-300 dark:hover:bg-[#020817] dark:border-[#020817] border-b-4" 
+                  key={item.id}
+                  onClick={() => props.selectHistory(item.id)}
+                >
+                  {item.text}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
